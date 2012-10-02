b0039837cc03fc25cf0755ef11fa7ec77c4f1abe
Scott Lookabill <scott.lookabill@gmail.com>
2012-10-01 21:31:45 -0500

    jQuery UI Dialog Upgrades
    Siteaward, and skill admin

--- siteawardadmin.php
+++ siteawardadmin.php
@@ -38,8 +38,7 @@ elseif($_GET['edit']) {
   if($rows) {
     $values = mysql_fetch_row($result);
     ?>
-<div id="editDiv" class="ajaxForm" title="Edit Site Awards">
-    <form id="editForm" method="POST" onSubmit="postEdit(); return false;">
+  <form id="editForm" method="POST">
     <input type="hidden" name="id" value="<?=$id?>" />
       <table>
         <tr>
@@ -72,17 +71,8 @@ elseif($_GET['edit']) {
           <td><label for="link">Link: </label></td>
           <td><input type="text" name="link" id="link" value="<?=stripslashes($values[6])?>"></td>
         </tr>
-        <tr>
-          <td colspan="2" align="center">
-            <input type="submit" id="Submit" name="Submit" value="Submit" />
-            <input type="reset" id="Reset" name="Reset" />
-            <input type="button" id="Cancel" name="Cancel" value="Cancel"
-                 onClick="$('#Reset').click();destroyForm();" />
-          </td>
-        </tr>
       </table>
     </form>
-</div>
 <script type="text/javascript">
 $(function() {
     $("#date_edit").datepicker(
@@ -147,12 +137,12 @@ else {
   <div id="message" style="color: green" ></div>
   <div id="response"></div>
   <p>
-      <a name="adddialog" onclick="$('#add').show()">
+      <a onClick="$('#add-form').dialog('open');" href="#">
           <span style="color:#6699CC;">Add New Site Award</span>
       </a>
   </p>
-  <div class="ajaxForm" style="display:none;" id="add" title="Add New Site Award">
-  <form id="addForm" method="POST" onSubmit="postAdd(); return false;">
+  <div id="add-form" title="Add New Site Award">
+  <form id="addForm" method="POST">
     <table>
       <tr>
         <td><label for="name">Name: </label></td>
@@ -181,20 +171,13 @@ else {
         <td><label for="link">Link: </label></td>
         <td><input type="text" name="link" id="link"></td>
       </tr>
-      <tr>
-        <td colspan="2" align="center">
-          <input type="submit" id="Submit" name="Submit" value="Submit" />
-          <input type="reset" id="Reset" name="Reset" value="Reset" />
-          <input type="button" id="Cancel" name="Cancel" value="Cancel"
-                 onClick="$('#Reset').click();$('#add').hide();" />
-        </td>
-      </tr>
     </table>
   </form>
   </div>
 
-  <div id="editdialog" title="Edit Site Award" >
-
+  <div id="editArea" title="Edit Site Award">
+    <form id="editForm" method="POST">
+    </form>
   </div>
 
   <div id="datatable"></div>
@@ -207,19 +190,9 @@ else {
 
   function getEditForm(id) {
     $.get("<?=$_SERVER['PHP_SELF']?>?edit="+id,{},function(data){
-        if ($("#editArea").length < 1){
-            makeDiv("editArea","editArea","body","display:none;");
-        }
-        $("#editArea").html(data);
-        dressAjaxForm("editDiv");
-        $("#editArea").show();
-    },'html');
-  }
-
-  function destroyForm(){
-      $("#editArea").hide('fast',function(){
-        $("#editArea").remove();
-        getDataTable();
+      $("#editArea").html(data);
+    },'html').complete(function() {
+      $("#editArea").dialog("open");
       });
   }
 
@@ -238,7 +211,6 @@ else {
         success: showSuccess
     }
     $("#addForm").ajaxSubmit(options);
-    $("#Cancel").click();
     return false;
   }
 
@@ -248,7 +220,6 @@ else {
         success: showSuccess
     }
     $("#editForm").ajaxSubmit(options);
-    destroyForm();
     return false;
   }
 
@@ -258,6 +229,45 @@ else {
     },'html');
   }
   $(document).ready(getDataTable);
+
+  $(function() {
+    $("#add-form").dialog({
+        autoOpen: false,
+        width: 550,
+        modal: true,
+        buttons: {
+          "Submit": function() {
+            postAdd();
+            $( this ).dialog( "close" );
+            },
+          Cancel: function() {
+            $( this ).dialog( "close" );
+            }
+          },
+        close: function() {
+          document.forms["addForm"].reset();
+          }
+      });
+
+      $("#editArea").dialog({
+        autoOpen: false,
+        width: 550,
+        modal: true,
+        buttons: {
+          "Submit": function() {
+            postEdit();
+            $( this ).dialog( "close" );
+            },
+          Cancel: function() {
+            $( this ).dialog( "close" );
+            }
+          },
+          close: function() {
+            document.forms["editForm"].reset();
+            }
+        });
+  });
+
 </script>
  <?php
   include_once("footer.php");
--- skilladmin.php
+++ skilladmin.php
@@ -38,8 +38,7 @@ elseif($_GET['edit']) {
   if($rows) {
     $values = mysql_fetch_row($result);
     ?>
-<div id="editDiv" class="ajaxForm" title="Edit Skill">
-    <form id="editForm" method="POST" onSubmit="postEdit(); return false;">
+  <form id="editForm" method="POST">
     <input type="hidden" name="id" value="<?=$id?>" />
       <table>
         <tr>
@@ -54,17 +53,8 @@ elseif($_GET['edit']) {
           <td><label for="ico">Icon: </label></td>
           <td><input type="text" name="ico" id="ico" value="<?=stripslashes($values[3])?>"></td>
         </tr>
-        <tr>
-          <td colspan="2" align="center">
-            <input type="submit" id="Submit" name="Submit" value="Submit" />
-            <input type="reset" id="Reset" name="Reset" />
-            <input type="button" id="Cancel" name="Cancel" value="Cancel"
-                 onClick="$('#Reset').click();destroyForm();" />
-          </td>
-        </tr>
       </table>
     </form>
-</div>
 <?php
     }
   }
@@ -110,12 +100,12 @@ else {
   <div id="message" style="color: green" ></div>
   <div id="response"></div>
   <p>
-      <a name="adddialog" onclick="$('#add').show()">
+      <a onClick="$('#add-form').dialog('open');" href="#">
           <span style="color:#6699CC;">Add New Skill</span>
       </a>
   </p>
-  <div class="ajaxForm" style="display:none;" id="add" title="Add New Skill">
-  <form id="addForm" method="POST" onSubmit="postAdd(); return false;">
+  <div id="add-form" title="Add New Skill">
+  <form id="addForm" method="POST">
     <table>
       <tr>
         <td><label for="name">Name: </label></td>
@@ -129,20 +119,13 @@ else {
         <td><label for="ico">Icon Image file location: </label></td>
         <td><input type="text" name="ico" id="ico"></td>
       </tr>
-      <tr>
-        <td colspan="2" align="center">
-          <input type="submit" id="Submit" name="Submit" value="Submit" />
-          <input type="reset" id="Reset" name="Reset" value="Reset" />
-          <input type="button" id="Cancel" name="Cancel" value="Cancel"
-                 onClick="$('#Reset').click();$('#add').hide();" />
-        </td>
-      </tr>
     </table>
   </form>
   </div>
 
-  <div id="editdialog" title="Edit Skill" >
- 
+  <div id="editArea" title="Edit Skill">
+    <form id="editForm" method="POST">
+    </form>
   </div>
 
   <div id="datatable"></div>
@@ -150,22 +133,12 @@ else {
   <script type="text/javascript">
   function getEditForm(id) {
     $.get("<?=$_SERVER['PHP_SELF']?>?edit="+id,{},function(data){
-        if ($("#editArea").length < 1){
-            makeDiv("editArea","editArea","body","display:none;");
-        }
-        $("#editArea").html(data);
-        dressAjaxForm("editDiv");
-        $("#editArea").show();
-    },'html');
-  }
-
-  function destroyForm(){
-      $("#editArea").hide('fast',function(){
-        $("#editArea").remove();
-        getDataTable();
+      $("#editArea").html(data);
+    },'html').complete(function() {
+      $("#editArea").dialog("open");
       });
   }
-  
+
   function del(id) {
     $.get("<?=$_SERVER['PHP_SELF']?>?del="+id,{},showSuccess,'html');
   }
@@ -181,7 +154,6 @@ else {
         success: showSuccess
     }
     $("#addForm").ajaxSubmit(options);
-    $("#Cancel").click();
     return false;
   }
 
@@ -191,7 +163,6 @@ else {
         success: showSuccess
     }
     $("#editForm").ajaxSubmit(options);
-    destroyForm();
     return false;
   }
 
@@ -201,6 +172,45 @@ else {
     },'html');
   }
   $(document).ready(getDataTable);
+
+  $(function() {
+    $("#add-form").dialog({
+        autoOpen: false,
+        width: 550,
+        modal: true,
+        buttons: {
+          "Submit": function() {
+            postAdd();
+            $( this ).dialog( "close" );
+            },
+          Cancel: function() {
+            $( this ).dialog( "close" );
+            }
+          },
+        close: function() {
+          document.forms["addForm"].reset();
+          }
+      });
+
+      $("#editArea").dialog({
+        autoOpen: false,
+        width: 550,
+        modal: true,
+        buttons: {
+          "Submit": function() {
+            postEdit();
+            $( this ).dialog( "close" );
+            },
+          Cancel: function() {
+            $( this ).dialog( "close" );
+            }
+          },
+          close: function() {
+            document.forms["editForm"].reset();
+            }
+        });
+  });
+
 </script>
  <?php
   include_once("footer.php");
